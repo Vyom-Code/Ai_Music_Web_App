f2ac0657790633b4bcca649a9ffaa33deb41a51c
Vyom-Code <82333325+Vyom-Code@users.noreply.github.com>
2022-05-15 10:54:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song1="";
+song2="";
+function preload(){
+    song1= loadSound("music.mp3");
+    song2= loadSound("music.mp3(2).mp3");
+}
+
+function setup(){
+    canvas = createCanvas(850,600);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 650, 800);
+}
